57002a4a109a2d7546c57177ffdb57c66c6ec1fe
“nzeki” <nzekisyntax@gamil.com>
2022-01-29 11:53:39 -0500

    Feature Sun with errors to be resolved

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,10 +3,16 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        SolarSystem milkway=new SolarSystem();
-        milkway.sun=1;
-        milkway.moon=2;
-        milkway.planets=3;
+        // write your code here
+        SolarSystem milkway = new SolarSystem();
+        milkway.sun = 1;
+        milkway.moon = 2;
+        milkway.planets = 3;
+
+        sun solara = new sun();
+        Solana.Heat = "677777j";
+        Solana.redius = "327687428736";
+        Solana.remp = 1224566 C;
     }
 }
+
